044d9bd1e0c5001d94d7a50efb1d0ed0b498abb5
jwenger100 <jon.wenger@gmail.com>
2022-02-01 18:37:49 +0000

    2.4.5.6

--- lib/avalara_sdk/version.rb
+++ lib/avalara_sdk/version.rb
@@ -1,11 +1,13 @@
 =begin
-#Avalara Shipping Verification for Beverage Alcohol
+#Avalara Shipping Verification only
 
 #API for evaluating transactions against direct-to-consumer Beverage Alcohol shipping regulations.  This API is currently in beta. 
 
+SDK Version : 2.4.5.6
+
+
 =end
 
-
 module AvalaraSdk
-  VERSION = '22.1.0'
+  VERSION = '2.4.5.6'
 end
